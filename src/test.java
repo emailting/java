aa7466997a80c36c512f6a4b57c75eabc09776a2
emailting <marlboroting@outlook.com>
2020-02-12 21:01:31 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("one");
         System.out.println("Two");
+        System.out.println("在GIT上修改");
     }
 }
